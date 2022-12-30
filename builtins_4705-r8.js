e7f8e2f735e7e02b3a2be6301ec2d04b88900329
Vitaly Pronkin <vit@mifki.com>
2022-12-30 13:35:09 +1300

    add 0.47.05-r8 stuff

---
+++ builtins_4705-r8.js
@@ -0,0 +1,6 @@
+var base = require('./builtins_4412-r2');
+
+//TODO: temporary as it's not parsed correctly
+base.types.df.global.ui_menu_width = { _type:'number[]', _array:'number' },
+
+module.exports = base;
